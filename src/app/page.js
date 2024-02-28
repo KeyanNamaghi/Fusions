9f444dea9bbf903d22bcd6bbfae9d9e044c74c32
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-02-28 11:04:57 +0000

    LGTM

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,12 @@
 import React from 'react'
 import { Guesses } from '@/components/Guesses'
 import { FusionImage } from '@/components/FusionImage'
-import { gameNumber, gen1Pokemon } from '@/lib/utils'
-import { readFile } from 'fs/promises'
 
 async function getData() {
-  const filePath = './src/app/api/kanto/output.txt'
-  const raw = await readFile(filePath, 'utf-8')
-
-  const data = raw.split('\n')
-
-  const game = gameNumber() % data.length
-  const daily = data[game]
-  const [fusion, artist] = daily.split(',')
-  const [a, b] = fusion.split('.')
-
-  const nameA = gen1Pokemon[a]
-  const nameB = gen1Pokemon[b]
-
-  return { game, a, nameA, b, nameB, artist }
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+  const res = await fetch(`${protocol}://${process.env.VERCEL_URL}/api/kanto`)
+  const data = await res.json()
+  return data
 }
 
 export default async function Home() {
